1675cb8308063f91f3f1858e09c67258e35a883a
JosephEyler1 <josepheyler1@gmail.com>
2024-11-04 18:34:42 -0600

    Revert "Branch1"

--- Develop/client/src/App.tsx
+++ Develop/client/src/App.tsx
@@ -1,31 +1,14 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import './App.css';
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { setContext } from '@apollo/client/link/context';
 
-const link = createHttpLink({
-  uri: "/graphql"
-})
-const authLink = setContext((_, {headers}) => {
-  const token = localStorage.getItem("id_token")
-  return{
-    headers: { 
-      ... headers,
-      authorization: `Bearer ${token}`
-    }
-  }
-})
-const client = new ApolloClient({
-  link: authLink.concat(link),
-  cache: new InMemoryCache(),
-});
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-        <Navbar />
-        <Outlet />
-    </ApolloProvider>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
 
--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,43 +1,21 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import express, { Application, Request, Response} from 'express';
+import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
-import { typeDefs } from './schemas/typeDefs.js';
-import { resolvers } from './schemas/resolvers.js';
-import { authMiddleware } from './services/auth.js'; // Import auth middleware
+import routes from './routes/index.js';
 
-const app: Application = express();
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-async function startApolloServer() {
-  await server.start();
-  await db;
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.use("/graphql", expressMiddleware(server, {
-    context: authMiddleware as any
-  }));
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get("*", (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
-    })
-  };
-
-
-  
-    app.listen(PORT, () =>
-      console.log(
-        `🌍 Now listening on http://localhost:${PORT}/graphql`
-      )
-    );
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-startApolloServer();
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
